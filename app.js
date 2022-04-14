0e7b4d3fc35072e4656cbd5ff2b68630f3adde58
syarif taufik <syariftaufik11@gmail.com>
2022-04-14 16:35:47 +0700

    finished connected to database mongo and create data categories

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+//* use mongoose
+const mongoose = require("mongoose");
+const options = {
+  autoIndex: false, // Don't build indexes
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
+};
+
+mongoose.connect("mongodb://localhost:27017/Staycation", options);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,3 +1,5 @@
+const Category = require("../models/Category");
+
 module.exports = {
   viewDashboard: (req, res) => {
     res.render("admin/dashboard/view_dashboard");
@@ -13,5 +15,11 @@ module.exports = {
   },
   viewBooking: (req, res) => {
     res.render("admin/booking/view_booking");
+  },
+  addCategory: async (req, res) => {
+    const { name } = req.body;
+    console.log(name);
+    await Category.create({ name });
+    res.redirect("/admin/category");
   },
 };
